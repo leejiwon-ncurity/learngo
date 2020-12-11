0454a45e960e04a3103275c64a81d016e93d762c
leejiwon-ncurity <jwlee@ncurity.com>
2020-12-11 02:36:23 +0000

    #1.7 Switch

--- main.go
+++ main.go
@@ -6,10 +6,24 @@ import (
 
 func canIDrink(age int) bool {
 	// variable exprission : if-else에서 사용할 수 있는 변수를 앞에 선언해준다.
-	if koreanAge := age + 2; koreanAge < 18 {
+	// switch문에서도 사용할 수 있다.
+	switch koreanAge := age + 2; {
+	case 10:
 		return false
+	case 18:
+		return true
 	}
-	return true
+	return false
+
+	// switch koreanAge := age + 2; {
+	// case koreanAge < 18:
+	// 	return false
+	// case koreanAge == 18:
+	// 	return true
+	// case koreanAge > 50:
+	// 	return false
+	// }
+	// return false
 
 }
 
