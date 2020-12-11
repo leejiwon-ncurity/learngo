db22c7b068fd193021a907a05f04f663378db854
leejiwon-ncurity <jwlee@ncurity.com>
2020-12-11 02:30:27 +0000

    #1.6 If with a Twist - variable expression

--- main.go
+++ main.go
@@ -4,21 +4,15 @@ import (
 	"fmt"
 )
 
-// go에서 loop를 만드는 방법.
-// range는 항상 index를 return한다.
-func superAdd(numbers ...int) int {
-	// for index, number := range numbers {
-	// 	fmt.Println(index, number)
-	// }
-	total := 0
-	for _, number := range numbers {
-		fmt.Println(number)
-		total += number
+func canIDrink(age int) bool {
+	// variable exprission : if-else에서 사용할 수 있는 변수를 앞에 선언해준다.
+	if koreanAge := age + 2; koreanAge < 18 {
+		return false
 	}
-	return total
+	return true
+
 }
 
 func main() {
-	result := superAdd(1, 2, 3, 4, 5, 6, 7, 8)
-	fmt.Println(result)
+	fmt.Println(canIDrink(16))
 }
