43ef04efd9e3e35352db8c90ae4c854bb2524104
leejiwon-ncurity <jwlee@ncurity.com>
2020-12-11 04:14:14 +0000

    #1.11 Structs

--- main.go
+++ main.go
@@ -4,10 +4,15 @@ import (
 	"fmt"
 )
 
+type person struct {
+	name    string
+	age     int
+	favFood []string
+}
+
 func main() {
-	// map[key type] value type {key:value}
-	jiwon := map[string]string{"name": "jiwon", "age": "12"}
-	for _, value := range jiwon {
-		fmt.Println(value)
-	}
+	// struct는 object와 비슷하면서 map보다 유연한게 특징이다.
+	favFood := []string{"kimchi", "ramen"}
+	jiwon := person{name: "jiwon", age: 30, favFood: favFood}
+	fmt.Println(jiwon)
 }
