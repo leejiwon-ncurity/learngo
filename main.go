907f27e2e550766376d03b79c07344911bc64768
leejiwon-ncurity <jwlee@ncurity.com>
2020-12-11 04:34:29 +0000

    #2.0 Account + NewAccount

--- main.go
+++ main.go
@@ -2,17 +2,11 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/leejiwon-ncurity/learngo/accounts"
 )
 
-type person struct {
-	name    string
-	age     int
-	favFood []string
-}
-
 func main() {
-	// struct는 object와 비슷하면서 map보다 유연한게 특징이다.
-	favFood := []string{"kimchi", "ramen"}
-	jiwon := person{name: "jiwon", age: 30, favFood: favFood}
-	fmt.Println(jiwon)
+	account := accounts.NewAccount("jiwon")
+	fmt.Println(account)
 }
