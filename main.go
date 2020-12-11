bae775dc835a9bc5120d9f2c54f57c33b78ff281
leejiwon-ncurity <jwlee@ncurity.com>
2020-12-11 01:41:12 +0000

    #1.2 Variables and Constants

--- main.go
+++ main.go
@@ -3,7 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	const name string = "jiwon"
+	// 코드를 축약시켜서 사용하여 변수를 선언할 수 있다.
+	// 축약형은 오로지 func안에서만 가능하고 변수에만 적용할 수 있다.
+	// 축약형이 존재하면 Go가 첫번째 값을 기준으로 변수의 type을 찾아 정해줄것임.
+	// var name string = "jiwon"
+	name := "jiwon"
+
 	name = "Lynn"
 	fmt.Println(name)
 }
